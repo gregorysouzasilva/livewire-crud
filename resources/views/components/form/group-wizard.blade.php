1e3d3182435eba53c1c5c9e927ab9d1c4234c4de
Gregory Silva <gregorysouzasilva@hotmail.com>
2023-01-20 14:01:24 -0800

    fix checkbox

--- resources/views/components/form/group-wizard.blade.php
+++ resources/views/components/form/group-wizard.blade.php
@@ -3,7 +3,7 @@
 @endphp
 <div class="fv-row mb-10 col-{{$width ?? 4}}" id='div-{{$wirePrefix}}-{{$field}}' style="display: inline-block">
     @if ($type == 'checkbox')
-        <x-form.checkbox :field="$field" :options="$options ?? []" :wirePrefix="$wirePrefix" :value="$value ?? []" inputClass="form-control-solid" />
+        <x-form.checkbox :field="$field" :wirePrefix="$wirePrefix" :value="$value ?? 1" inputClass="form-control-solid" />
     @endif
     
     <label class="form-label" style="display: inline">@lang($label) @if(!empty($explanation))<i class="bi bi-info-circle-fill" title="{{$explanation}}"></i> @endif</label> <span style="float:right">{{ $slot }}</span>
