9d9a9ee27866ab1c5ed246430badb8512864e42d
Gregory Silva <gregorysouzasilva@hotmail.com>
2023-01-20 14:01:54 -0800

    fix checkbox

--- resources/views/components/form/group-checkbox.blade.php
+++ resources/views/components/form/group-checkbox.blade.php
@@ -4,7 +4,7 @@ $disabled = $disabled ?? 0;
 @endphp
 <!--begin::Input group-->
 <div class="mb-4 col-{{$width ?? 4}}" id='div-{{$wirePrefix}}-{{$field}}' style="align-self: center;">
-        <x-form.checkbox :field="$field" :options="$options ?? []" :wirePrefix="$wirePrefix" :value="$value ?? 1" inputClass="form-control-solid" />
+        <x-form.checkbox :field="$field" :wirePrefix="$wirePrefix" :value="$value ?? 1" inputClass="form-control-solid" />
         <label for="{{$field}}" class="form-label" style="display: inline"><span>@lang($label)</span></label>
         @error("$wirePrefix$field") <span class="invalid-feedback" style="display: block">@lang($message)</span>@enderror
 </div>
