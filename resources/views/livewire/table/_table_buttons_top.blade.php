19f78a971d14b0729d4e984fd90005df8bfc5a30
Gregory Silva <gregorysouzasilva@hotmail.com>
2023-03-07 16:57:47 -0800

    print button

--- resources/views/livewire/table/_table_buttons_top.blade.php
+++ resources/views/livewire/table/_table_buttons_top.blade.php
@@ -1,28 +1,45 @@
-@if(!empty($pageInfo['shortcuts']) && empty($condensed))
+@if (!empty($pageInfo['shortcuts']) && empty($condensed))
     <x-button.shortcut-pages :pages="$pageInfo['shortcuts']" icon="bi bi-list" label="Shortcuts" />
 @endif
 @php
-$hideCreateButton = $hideCreateButton ?? false;
+    $hideCreateButton = $hideCreateButton ?? false;
 @endphp
-@if(($pageInfo['permissions']['complete'] ?? false) && !in_array($contact->getCurrentStateRelation(class_basename($this->model))->status ?? null, ['completed', 'dismissed']))
-<button type="button" wire:click="confirmComplete('{{class_basename($this->model)}}')"
-    class="btn btn-light-danger me-3">
-    <i class="bi bi-lock-fill"></i>Complete
-</button>
-@if(hasRole('consultant'))
-<button type="button" wire:click="onPageDismiss('{{class_basename($this->model)}}')"
-    class="btn btn-light-danger me-3">
-    <i class="bi bi-lock-fill"></i>Dismiss
-</button>
+@if (
+    ($pageInfo['permissions']['complete'] ?? false) &&
+        !in_array($contact->getCurrentStateRelation(class_basename($this->model))->status ?? null, [
+            'completed',
+            'dismissed',
+        ]))
+    <button type="button" wire:click="confirmComplete('{{ class_basename($this->model) }}')"
+        class="btn btn-light-danger me-3">
+        <i class="bi bi-lock-fill"></i>Complete
+    </button>
+    @if (hasRole('consultant'))
+        <button type="button" wire:click="onPageDismiss('{{ class_basename($this->model) }}')"
+            class="btn btn-light-danger me-3">
+            <i class="bi bi-lock-fill"></i>Dismiss
+        </button>
+    @endif
+    
+@elseif(
+    ($pageInfo['permissions']['complete'] ?? false) &&
+        in_array($contact->getCurrentStateRelation(class_basename($this->model))->status ?? null, [
+            'completed',
+            'dismissed',
+        ]))
+    <button type="button" wire:click="confirmReopen('{{ class_basename($this->model) }}')"
+        class="btn btn-light-danger me-3">
+        <i class="bi bi-unlock-fill"></i>Reopen
+        {{ $contact->getCurrentStateRelation(class_basename($this->model))->status }}
+    </button>
 @endif
-@elseif(($pageInfo['permissions']['complete'] ?? false) && in_array($contact->getCurrentStateRelation(class_basename($this->model))->status ?? null, ['completed', 'dismissed']))
-<button type="button" wire:click="confirmReopen('{{class_basename($this->model)}}')"
-    class="btn btn-light-danger me-3">
-    <i class="bi bi-unlock-fill"></i>Reopen {{$contact->getCurrentStateRelation(class_basename($this->model))->status}}
-</button>
+@if (!empty($pageInfo['print_link'] ?? false))
+    <a href="$pageInfo['print_link']" class="btn btn-light-danger me-3" target="_blank">
+        <i class="bi bi-print"></i>Print
+        </button>
 @endif
 <!--begin::Add -->
-@if($pageInfo['permissions']['create'] && !$hideCreateButton)
-<x-button.create/>
+@if ($pageInfo['permissions']['create'] && !$hideCreateButton)
+    <x-button.create />
 @endif
 <!--end::Add -->
