697b2fcdad04a825095d4bf5d6b4a602f4a20bde
Gregory Silva <gregorysouzasilva@hotmail.com>
2023-08-12 21:34:33 -0700

    url select ajax

--- resources/views/components/form/group.blade.php
+++ resources/views/components/form/group.blade.php
@@ -9,11 +9,13 @@ if(!empty($options) && empty($type)) {
 }
 $type = $type ?? 'text';
 $validations = $validations ?? '';
+$url = $url ?? '';
 @endphp
 <!--begin::Input group-->
 <div class="form-floating mb-4 col-xl-{{$width}} col-md-{{$width+2}} col-sm-12" id='div-{{$wirePrefix}}-{{$field}}'>
         <x-dynamic-component component="{{'form.' . $type}}" :field="$field" :options="$options ?? []" :wirePrefix="$wirePrefix" :value="$value ?? []" 
-            :disabled="$disabled ?? false" :multiple="$multiple ?? false" :useIndex="$useIndex ?? false" label="{{$label}}" :hideLabel="$hideLabel" :validations="$validations">
+            :disabled="$disabled ?? false" :multiple="$multiple ?? false" :useIndex="$useIndex ?? false" label="{{$label}}" :hideLabel="$hideLabel" 
+            :validations="$validations" :url="$url">
             @if(!$hideLabel)
                 <label for="{{$field}}" @if($type=="memoeditor")style="margin-top:-17px;"@endif>@lang($label)</label>
             @endif
