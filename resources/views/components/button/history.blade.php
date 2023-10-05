87c878c8a61bc9228aa3e14cdf3f93f3515a0360
Gregory Silva <gregorysouzasilva@hotmail.com>
2023-10-05 14:14:02 -0700

    history button

--- resources/views/components/button/history.blade.php
+++ resources/views/components/button/history.blade.php
@@ -1,5 +1,5 @@
 @if(($service->serviceType->histories[$serviceTypeHistory] ?? false) && (!in_array($contact->getCurrentStateRelation($stateRelation)->status, ['dismissed']) || hasRole('consultant')))
-    <a href="{{route('clients.contacts.' . $routeName, ['client_uuid' => $client->uuid, 'contact_uuid' => $contact->uuid])}}" class="btn btn-light p-2 position-relative m-2" style="min-width: 50px;">
+    <a href="{{route('clients.contacts.' . $routeName, ['client_uuid' => $client->uuid, 'service_id' => $service->id, 'contact_uuid' => $contact->uuid])}}" class="btn btn-light p-2 position-relative m-2" style="min-width: 50px;">
         <i class="bi {{$icon}}"></i> <br>
         @lang($label) 
         <x-badge color="{{$contact->getRelationColor($stateRelation)}}" position='top' showZero=true>{{$contact->{$contactRelation}->count()}}</x-badge>
