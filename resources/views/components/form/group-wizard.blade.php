db3e02b91a63a34357b1a8469c4b49eefc016600
Gregory Silva <gregorysouzasilva@hotmail.com>
2024-03-06 18:33:29 -0800

    removed style group wizard

--- resources/views/components/form/group-wizard.blade.php
+++ resources/views/components/form/group-wizard.blade.php
@@ -3,7 +3,7 @@
 @endphp
 <div class="fv-row mb-10 col-{{$width ?? 4}}" id='div-{{$wirePrefix}}-{{$field}}' style="display: inline-block">
     @if ($type == 'checkbox')
-        <x-form.checkbox :field="$field" :wirePrefix="$wirePrefix" :value="$value ?? 1" inputClass="form-control-solid" />
+        <x-form.checkbox :field="$field" :wirePrefix="$wirePrefix" :value="$value ?? 1" />
     @endif
     
     <label class="form-label" style="display: inline">@lang($label)
@@ -11,7 +11,7 @@
         <span style="float:right">{{ $slot }}</span>
 
     @if ($type != 'checkbox')
-        <x-dynamic-component component="{{'form.' . $type}}" :field="$field" :options="$options ?? []" :wirePrefix="$wirePrefix" :value="$value ?? []" inputClass="form-control-solid" :useIndex="$useIndex ?? false"  />
+        <x-dynamic-component component="{{'form.' . $type}}" :field="$field" :options="$options ?? []" :wirePrefix="$wirePrefix" :value="$value ?? []" :useIndex="$useIndex ?? false"  />
     @endif
     @error($wirePrefix . $field)
         <div class="fv-plugins-message-container invalid-feedback"><div data-field="text_input" data-validator="notEmpty">{{ $message }}</div></div>
