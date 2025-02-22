512c8ea099999d1acb10fe57e9c41edb9da1c381
Gregory <gregory@Gregorys-MacBook-Air.local>
2025-02-21 16:23:33 -0800

    fixed uuid boot

--- src/Traits/UUID.php
+++ src/Traits/UUID.php
@@ -6,15 +6,8 @@ use Illuminate\Support\Str;
 
 trait UUID
 {
-    protected static function boot ()
+    protected static function bootUUID()
     {
-        // Boot other traits on the Model
-        parent::boot();
-
-        /**
-         * Listen for the creating event on the user model.
-         * Sets the 'id' to a UUID using Str::uuid() on the instance being created
-         */
         static::creating(function ($model) {
             if ($model->getKey() === null) {
                 $model->setAttribute($model->getKeyName(), Str::uuid()->toString());
