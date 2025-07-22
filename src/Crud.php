6b208f49f9e83544d52042885a9140fb1fa0942d
Gregory <gregory@Gregorys-MacBook-Air.local>
2025-07-22 11:22:32 -0700

    removed client and contact from header

--- src/Crud.php
+++ src/Crud.php
@@ -110,7 +110,7 @@ class Crud extends BaseComponent
         $this->AddFiltersToPrintAndExport();
 
         return view($this->viewPath . 'index', ['collection' => $this->collection])
-            ->layout('layout.demo6.master', ['client' => $this->client, 'contact' => $this->contact, 'pageInfo' => $this->pageInfo, 'pageHeader' => $this->pageHeader]);
+            ->layout('layout.demo6.master', ['pageInfo' => $this->pageInfo, 'pageHeader' => $this->pageHeader]);
     }
 
 
